0a52df393b721d8d9236c7f476d7a1a2757719b5
Jeanne <jeanne.bnvt@gmail.com>
2024-11-22 14:13:52 +0100

    reorganisation

--- exo1.py
+++ exo1.py
@@ -1,8 +1,10 @@
 from gurobipy import *
+from utils import variableExemple1
 
-
-def createExemple1():
-
+def variableExemple1():
+    """ Retourne la matrice de contraintes de contrainte, de second membre et les 
+    matrice des fonctions objectifs du scénario 1 et 2 de l'exemple 1, ainsi que le
+    nombre de scénarios, variables et contraintes du problème"""
     #Matrice des contraintes
     A=[60,10,15,20,25,20,5,15,20,60]
 
@@ -14,65 +16,88 @@ def createExemple1():
     #fonction objectif scenario 2
     C2 = [2,4,6,8,10,12,14,16,18,70]
     
-    #nombre de scénario
-    n= 2
-    #nombre de variables du problème d'origine
-    p=10
-    #nombre de contraintes du problème d'origine
-    q=1
-    return A,B,C1,C2, n, p,q
-
-A,B,C1,C2,n,p,q = createExemple1()
+    return A,B,C1,C2
 
 
-m1 = Model("scenario 1")
-m2 = Model("scenario 2")
+def modelExemple1():
+    """Créer et retourne les deux modèles qui correspondent au scénario 1 et 2 
+    de l'exemple 1"""
+    A,B,C1,C2 = variableExemple1()
 
-#declaration variables de decision
-x1 = []
-for i in range(p):
-    x1.append(m1.addVar(vtype=GRB.BINARY, name="x%d" % (i+1)))
-x2 = []
-for i in range(p):
-    x2.append(m2.addVar(vtype=GRB.BINARY, name="x%d" % (i+1)))
-m1.update()
-m2.update()
+    m1 = Model("scenario 1")
+    m2 = Model("scenario 2")
+    p= len(C1)
+    #declaration variables de decision
+    x1 = []
+    for i in range(p):
+        x1.append(m1.addVar(vtype=GRB.BINARY, name="x%d" % (i+1)))
+    x2 = []
+    for i in range(p):
+        x2.append(m2.addVar(vtype=GRB.BINARY, name="x%d" % (i+1)))
+    m1.update()
+    m2.update()
 
-#definition de l'objectif en fonction du scenario choisis
-obj1 = LinExpr();
-obj2 = LinExpr();
+    #definition de l'objectif en fonction du scenario choisis
+    obj1 = LinExpr();
+    obj2 = LinExpr();
 
-obj1 = 0
-obj2 = 0
+    obj1 = 0
+    obj2 = 0
 
-for j in range(p):
-    obj1 += C1[j]*x1[j]
-    obj2 += C2[j]*x2[j]
+    for j in range(p):
+        obj1 += C1[j]*x1[j]
+        obj2 += C2[j]*x2[j]
+
+    m1.setObjective(obj1, GRB.MAXIMIZE)
+    m2.setObjective(obj2, GRB.MAXIMIZE)
+
+    #definition des contrainte
+    m1.addConstr(quicksum(A[i]*x1[i] for i in range(p)) <= B)
+    m2.addConstr(quicksum(A[i]*x2[i] for i in range(p)) <= B)
+
+    print(m1.NumVars)
+    return m1, m2, x1 , x2
 
 
 
-m1.setObjective(obj1, GRB.MAXIMIZE)
-m2.setObjective(obj2, GRB.MAXIMIZE)
+def solveExemple1():
+    """Résout les deux programmes linéaires de l'Exemple 1 celui du scénario 1
+    et du scénario deux et renvoie les valeurs à l'objectif ainsi que le vecteur de 
+    conséquence"""
+    m1,m2,x1,x2= modelExemple1()
+    #Resolution
+    m1.optimize()
+    m2.optimize()
+    
+    #recuperation solution optimal
+    x1_opt = [round(var.x) for var in x1]
+    x2_opt = [round(var.x) for var in x2]
 
-#definition des contrainte
-m1.addConstr(quicksum(A[i]*x1[i] for i in range(p)) <= B)
-m2.addConstr(quicksum(A[i]*x2[i] for i in range(p)) <= B)
 
-#Resolution
-m1.optimize()
-m2.optimize()
-print("")
-print("Solution optimale dans le scenario 1:")
-for j in range(p):
-    print('x%d'%(j+1), '=', x1[j].x)
-print("")
-print('Valeur de la fonction objectif dans le scenario 2:', m1.objVal)
+    #recuperation du vecteur de consequence optimal
+    obj1 = m1.ObjVal
+    obj2 = m2.ObjVal
 
-   
-print("")
-print("Solution optimale dans le scenario 2:")
-for j in range(p):
-    print('x%d'%(j+1), '=', x2[j].x)
-print("")
-print('Valeur de la fonction objectif :', m2.objVal)
+    return x1_opt, x2_opt, obj1, obj2
+
+
+def affichageExempl1():
+    x1_opt, x2_opt, obj1, obj2 = solveExemple1()
+    
+    print("-----------------------------------------")
+    print("-----------------------------------------")
+    print("")
+    print("Solution optimale dans le scenario 1:")
+    print(x1_opt)
+    print("")
+    print('Valeur de la fonction objectif dans le scenario 1:', obj1 )
+
+    
+    print("")
+    print("Solution optimale dans le scenario 2:")
+    print(x2_opt)
+    print("")
+    print('Valeur de la fonction objectif :', obj2)
+    print("-----------------------------------------")
+    print("-----------------------------------------")
 
---
+++ minmaxregret.py
@@ -0,0 +1,5 @@
+from gurobipy import *
+import numpy as np
+from pylab import plot, show
+from exo1 import modelExemple1
+
