9d02d4567e834348f33ddfa5c244085feb34a75d
Jeanne <jeanne.bnvt@gmail.com>
2024-11-24 18:38:24 +0100

    ajout du main

---
+++ main.py
@@ -0,0 +1,58 @@
+from exo1 import affichageExempl1, variableGen, modelGen, solveGen, afficheGen
+from maxmin import affichageMaxMin as affichageMaxMinMax
+from minmaxregret import affichageMaxMin as affichageMinMaxRegret
+from visualisation import visualisation_Ex1
+
+
+def main():
+    
+    print("1. Résolution de l'exemple 1 (scénarios 1 et 2)")
+    print("2. Résolution avec le critère MaxMin de l'exemple 1")
+    print("3. Résolution avec le critère MinMax Regret de l'exemple 1")
+    print("4. Visualisation des solutions de l'exemple 1")
+    print("5. Résolution d'un problème généralisé")
+    
+    choix = input("Entrez le numéro de votre choix : ")
+    
+    if choix == "1":
+        print("\nRésolution de l'exemple 1 :")
+        affichageExempl1()
+        
+    elif choix == "2":
+        print("\nRésolution avec le critère MaxMin :")
+        affichageMaxMinMax()
+    elif choix == "3":
+        print("\nRésolution avec le critère MinMax Regret :")
+        affichageMinMaxRegret()
+    elif choix == "4":
+        print("\nVisualisation des solutions de l'exemple 1")
+        visualisation_Ex1()
+    elif choix == "5":
+        print("\nRésolution d'un problème généralisé :")
+        pb_generalise()
+
+    else:
+        print("\nChoix invalide. Veuillez entrer un numéro entre 0 et 4.")
+
+def pb_generalise():
+    """
+    Scenario pour resoudre un probleme generalise où l'utilisateur entre les parametres desires
+    """
+    n = int(input("nombre de scénarios (n) : "))
+    p = int(input("nombre de projets (p) : "))
+    
+    # generation des donnees
+    A, B, C = variableGen(n, p)
+    
+    # creation des modeles
+    models, variables = modelGen(A, B, C)
+    
+    # resolution des modeles
+    var_opt, objs = solveGen(models, variables)
+    
+    # affichage
+    afficheGen(var_opt, objs)
+
+
+if __name__ == "__main__":
+    main()
--- visualisation.py
+++ visualisation.py
@@ -3,34 +3,37 @@ from exo1 import solveExemple1, variableExemple1
 from maxmin import solve_MaxMin
 from minmaxregret import solve_MinMaxRegret
 
-_,_,C1,C2= variableExemple1()
+def visualisation_Ex1():
+    """Representation dans le plan des differentes solutions de l'exemple 1
+    """
+    _,_,C1,C2= variableExemple1()
 
-#Recuperation des coordonnees des point
-x1_star, x2_star, z1_x1_star,z2_x2_star = solveExemple1()
-z2_x1_star = sum([x1_star[i]*C2[i] for i in range(len(x1_star)) ])
-z1_x2_star = sum([x2_star[i]*C1[i] for i in range(len(x2_star)) ])
+    #Recuperation des coordonnees des point
+    x1_star, x2_star, z1_x1_star,z2_x2_star = solveExemple1()
+    z2_x1_star = sum([x1_star[i]*C2[i] for i in range(len(x1_star)) ])
+    z1_x2_star = sum([x2_star[i]*C1[i] for i in range(len(x2_star)) ])
 
-_,z_x_star,_ = solve_MaxMin()
+    _,z_x_star,_ = solve_MaxMin()
 
-_,z_x_star_prime,_ = solve_MinMaxRegret()
+    _,z_x_star_prime,_ = solve_MinMaxRegret()
 
-z1 = [z1_x1_star, z1_x2_star, z_x_star[0], z_x_star_prime[0]]
-z2 = [z2_x1_star, z2_x2_star, z_x_star[1], z_x_star_prime[1]]
+    z1 = [z1_x1_star, z1_x2_star, z_x_star[0], z_x_star_prime[0]]
+    z2 = [z2_x1_star, z2_x2_star, z_x_star[1], z_x_star_prime[1]]
 
-print(z1[0], z2[0])
-print(z1[1], z2[1])
+    print(z1[0], z2[0])
+    print(z1[1], z2[1])
 
-plt.scatter(z1[0], z2[0], color='red', label= r'$z(x_1^*)$ solution optimale dans le scénario 1')
-plt.scatter(z1[1], z2[1], color='blue', label= r'$z(x_2^*)$ solution optimale dans le scénario 2')
-plt.scatter(z1[2], z2[2], color='green', label= r'$z(x^*)$ critère maxmin')
-plt.scatter(z1[3], z2[3], color='orange', label= r"$z(x^*')$ critère minmax regret")
-plt.plot([z1[0], z1[1]], [z2[0], z2[1]], alpha = 0.3, color='purple', linestyle='--')
+    plt.scatter(z1[0], z2[0], color='red', label= r'$z(x_1^*)$ solution optimale dans le scénario 1')
+    plt.scatter(z1[1], z2[1], color='blue', label= r'$z(x_2^*)$ solution optimale dans le scénario 2')
+    plt.scatter(z1[2], z2[2], color='green', label= r'$z(x^*)$ critère maxmin')
+    plt.scatter(z1[3], z2[3], color='orange', label= r"$z(x^*')$ critère minmax regret")
+    plt.plot([z1[0], z1[1]], [z2[0], z2[1]], alpha = 0.3, color='purple', linestyle='--')
 
 
-plt.xlabel(r'$z_1(x)$')
-plt.ylabel(r'$z_2(x)$')
-# plt.title('Représentation des solutions dans le plan $(z_1, z_2)$')
-plt.legend()
+    plt.xlabel(r'$z_1(x)$')
+    plt.ylabel(r'$z_2(x)$')
+    # plt.title('Représentation des solutions dans le plan $(z_1, z_2)$')
+    plt.legend()
 
-plt.grid(True)
-plt.show()
+    plt.grid(True)
+    plt.show()
